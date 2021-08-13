e591198e59353d7c1ca0b6e3ed54e76d46545f58
Maxwell Beck <max@ryt.one>
2021-08-13 16:08:25 -0500

    Initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# sbvh
+
+A Rust implementation of Split Bounding Volume Hierarchies described in the paper [Spatial Splits in Bounding Volume Hierarchies](https://www.nvidia.in/docs/IO/77714/sbvh.pdf) by Martin Stich, Heiko Freidrich, and Andreas Dietrich.
---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "sbvh";
+  
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+        pkgs = import nixpkgs { inherit system; overlays = [ rust-overlay.overlay ]; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = let
+            rust = pkgs.rust-bin.stable.latest.default; 
+          in [
+            (rust.override { extensions = [ "rust-src" ]; })
+          ];
+        };
+      }
+    );
+}
